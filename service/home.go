914b2d570c513c2c3375fabe6f30101e2557a977
liziqiao <1240148677@qq.com>
2017-11-22 15:46:20 +0800

    Update home.go

--- service/home.go
+++ service/home.go
@@ -11,9 +11,9 @@ func homeHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		formatter.HTML(w, http.StatusOK, "index", struct {
 			ID      string `json:"id"`
-			AGE     string `json:"age"`
+			Name     string `json:"name"`
 			Content string `json:"content"`
 
-		}{ID: "15331151", AGE:"20", Content: "Nice to meet you!"})
+		}{ID: "15331176", Name:"liziqiao", Content: "welcome to Go!"})
 	}
 }
